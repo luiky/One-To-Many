88d9ce6755d8fbbeb774457f2d557cc135e01e06
luiky <kalderabatera@gmail.com>
2022-10-26 21:17:48 +0200

    minor bugs fixed

--- src/main/java/com/example/data/repository/DireccionRepository.java
+++ src/main/java/com/example/data/repository/DireccionRepository.java
@@ -1,9 +1,11 @@
 package com.example.data.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.data.model.Direccion;
 
+@Repository
 public interface DireccionRepository extends CrudRepository<Direccion, Long> {
 
 }
